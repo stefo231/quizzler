af20c724e78b858a082d30855241f9e6f07aa9b8
Stefan Zdravkov <szdravkov93@gmail.com>
2020-10-05 09:50:28 +0200

    refactoring

--- lib/question.dart
+++ lib/question.dart
@@ -2,9 +2,12 @@ class Question {
   String questionText;
   bool questionAnswer;
 
-  Question(String q, bool a) {
-    //Constructor
-    questionText = q;
-    questionAnswer = a;
-  }
+  // Question(String q, bool a) {
+  //   //Constructor
+  //   questionText = q;
+  //   questionAnswer = a;
+  // }
+
+  //! The same constructor but different approach!
+  Question(this.questionText, this.questionAnswer);
 }
